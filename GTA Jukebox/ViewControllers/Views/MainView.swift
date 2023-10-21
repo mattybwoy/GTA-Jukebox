f89676809fe1c0745618d58736a6bb3597ff7412
Matthew Lock <matthew_lock20@hotmail.com>
2023-10-21 15:54:43 +0100

    Added logo game selection logo and buttons

--- GTA Jukebox/ViewControllers/Views/MainView.swift
+++ GTA Jukebox/ViewControllers/Views/MainView.swift
@@ -47,6 +47,42 @@ class MainView: UIView {
             antennaImage.heightAnchor.constraint(equalToConstant: 130),
             antennaImage.widthAnchor.constraint(equalToConstant: 130)
         ])
+        
+        addSubview(gameSelectionImage)
+        gameSelectionImage.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            gameSelectionImage.centerXAnchor.constraint(equalTo: centerXAnchor),
+            gameSelectionImage.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 200),
+            gameSelectionImage.heightAnchor.constraint(equalToConstant: 150),
+            gameSelectionImage.widthAnchor.constraint(equalToConstant: 200)
+        ])
+        
+        addSubview(leftButton)
+        leftButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            leftButton.centerXAnchor.constraint(equalTo: centerXAnchor, constant: -120),
+            leftButton.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 200),
+            leftButton.heightAnchor.constraint(equalToConstant: 60),
+            leftButton.widthAnchor.constraint(equalToConstant: 60)
+        ])
+        
+        addSubview(rightButton)
+        rightButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            rightButton.centerXAnchor.constraint(equalTo: centerXAnchor, constant: 120),
+            rightButton.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 200),
+            rightButton.heightAnchor.constraint(equalToConstant: 60),
+            rightButton.widthAnchor.constraint(equalToConstant: 60)
+        ])
+        
+        addSubview(select)
+        select.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            select.centerXAnchor.constraint(equalTo: centerXAnchor),
+            select.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 320),
+            select.heightAnchor.constraint(equalToConstant: 80),
+            select.widthAnchor.constraint(equalToConstant: 130)
+        ])
     }
     
     private let titleImage: UIImageView = {
@@ -67,7 +103,43 @@ class MainView: UIView {
     private let antennaImage: UIImageView = {
         let antennaImage = UIImageView()
         antennaImage.image = UIImage(named: "antenna")
+        UIImageView.animate(withDuration: 0.8, delay: 0.5, options: [.repeat, .autoreverse]) {
+            antennaImage.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+        }
         return antennaImage
+    }()
+    
+    private var gameSelectionImage: UIImageView = {
+        let gameImage = UIImageView()
+        gameImage.image = UIImage(named: "GTA3Logo")
+        return gameImage
+    }()
+    
+    private let leftButton: UIImageView = {
+       let buttonImage = UIImageView()
+        buttonImage.image = UIImage(systemName: "play.circle")
+        buttonImage.transform = CGAffineTransform(scaleX: -1, y: 1)
+        buttonImage.tintColor = .white
+        return buttonImage
+    }()
+    
+    private let rightButton: UIImageView = {
+       let buttonImage = UIImageView()
+        buttonImage.image = UIImage(systemName: "play.circle")
+        buttonImage.tintColor = .white
+        return buttonImage
+    }()
+    
+    private let select: UIButton = {
+       let selectButton = UIButton()
+        selectButton.setTitle("Select", for: .normal)
+        selectButton.titleLabel?.font = UIFont(name: "PricedownBl-Regular", size: 30)
+        selectButton.titleLabel?.tintColor = .white
+        selectButton.backgroundColor = .clear
+        selectButton.layer.cornerRadius = 8
+        selectButton.layer.borderWidth = 3
+        selectButton.layer.borderColor = UIColor.systemGreen.cgColor
+        return selectButton
     }()
     
 }
